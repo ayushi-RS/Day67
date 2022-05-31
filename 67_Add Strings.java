0dc0b3bc1c79089ba0823fd0f19eea33d07803b6
Ayushi Malviya <102866923+ayushi-RS@users.noreply.github.com>
2022-06-01 00:03:07 +0530

    Add files via upload

---
+++ 67_Add Strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String addStrings(String num1, String num2) {        
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length()-1, j = num2.length()-1;
+        while(i>=0 || j>=0 || carry == 1){
+            int n1 = i>=0 ? (num1.charAt(i--)-'0') : 0;
+            int n2 = j>=0 ? (num2.charAt(j--)-'0') : 0;
+            int sum = n1+n2+carry;
+            sb.append(sum%10);
+            carry = sum/10;
+        }
+        return sb.reverse().toString();
+        
+    }
+}
